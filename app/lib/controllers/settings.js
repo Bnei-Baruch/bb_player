289c4449874268b85a24e664e42b582b6cc9e1dc
Rami Grossman <ramigg@gmail.com>
2013-02-12 13:31:19 +0200

    technology detection draft

--- app/lib/controllers/settings.js
+++ app/lib/controllers/settings.js
@@ -2,7 +2,16 @@ App.SettingsController = Ember.Controller.extend({
   playerWidth: 640,
   playerHeight: 480,
   defaultLanguage: 'he',
+  prefferedPlayer: 'jwplayer', // support for 'flowplayer' or 'jwplayer'
+  supportHLSFlashMode: false, //only when jwplayer is used
+  techPriorities: ['hls', 'flash', 'icecast'],
   technologies: [1, 2],
-  channelName: 'tv66'
+  channelName: 'tv66',
+
+  needs: ['technologyDetection'],
+  technologyDetection: function() {
+    return this.controllerFor('technologyDetection');
+  }.property(),
+
 });
 
--- app/lib/libs/technology-detect.js
+++ app/lib/libs/technology-detect.js
@@ -1,13 +1,18 @@
-App.TechnologyDetection = Ember.Object.extend({
+App.TechnologyDetection = Ember.Controller.extend({
   flashSupport: function(){
     // flowplayer has flash detection property
     return flowplayer.support.flashVideo;
   }.property(),
   hlsSupport: function(){
-    //iOS or safari
-    return App.BrowserDetect.browser == 'Safari' || MobileEsp.IsIphone;
+    //iOS or safari or non mobile hls flash mode
+    return App.BrowserDetect.browser == 'Safari' || MobileEsp.IsIphone || this.hlsFlashModeSupport;
   }.property(),
   icecastSupport: function(){
     return true;
+  }.property(),
+  //jwplayer supports playing hls stream in flash mode - this setting is enabled in settings
+  hlsFlashModeSupport: function(){
+    return this.flashSupport && !(MobileEsp.IsTierIphone || MobileEsp.IsTierTablet) && this.get('settings.supportHLSFlashMode');
   }.property()
+
 });
